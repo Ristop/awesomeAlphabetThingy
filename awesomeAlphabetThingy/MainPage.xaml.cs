7ea81ff2e96b0068a254d691d068335dc2752f4c
Risto Pärnapuu <ristoparnapuu@gmail.com>
2015-12-12 14:56:52 +0200

    repeat button

--- awesomeAlphabetThingy/MainPage.xaml.cs
+++ awesomeAlphabetThingy/MainPage.xaml.cs
@@ -19,26 +19,24 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace awesomeAlphabetThingy
-{
+namespace awesomeAlphabetThingy {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
+    public sealed partial class MainPage: Page {
         Dictionary<char, int> letters;
         List<char> keys;
         MediaElement mediaElement = new MediaElement();
         char current;
-        public MainPage()
-        {
+        Boolean playAgain;
+
+        public MainPage() {
             this.InitializeComponent();
-            
+
             Setup();
             Intro();
         }
-        public async void Intro()
-        {
+        public async void Intro() {
             await Task.Delay(2000);
             Say("Hello and welcome to the awesome alphabet thingy!");
             await Task.Delay(3000);
@@ -48,40 +46,42 @@ namespace awesomeAlphabetThingy
             await Task.Delay(2000);
             Next();
         }
-        public async void Next()
-        {
+        public async void Next() {
             Random rnd = new Random();
-            int index = rnd.Next(0, keys.Count - 1);
-            this.current = keys[index];
-            letterBox.Text = "?";
-            var mp3 = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///sounds/" + this.current.ToString() + ".mp3"));
+            int index = rnd.Next(0, keys.Count-1);
+            letterBox.Text="?";
+            this.current=keys[index];
+            Play(current);
+        }
+
+        public async void Play(char current) {
+
+            var mp3 = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///sounds/"+this.current.ToString()+".mp3"));
             var player = new AudioPlayer();
             await player.LoadFileAsync(mp3);
-            player.Play(this.current.ToString() + ".mp3", 0.5);
+            player.Play(this.current.ToString()+".mp3", 0.5);
             await Task.Delay(2000);
 
         }
-        public void Setup()
-        {
-            this.keys = new List<char>();
-            this.letters = new Dictionary<char, int>
+
+        public void Setup() {
+            this.keys=new List<char>();
+            this.letters=new Dictionary<char, int>
         {
             {'a', 2}, {'b', 2}, {'c', 2}, {'d', 2}, {'e', 2}, {'f', 2}, {'g', 2}, {'h', 2}, {'i', 2}, {'j', 2},
             {'k', 2}, {'l', 2}, {'m', 2}, {'n', 2}, {'o', 2}, {'p', 2}, {'q', 2}, {'r', 2}, {'s', 2}, {'t', 2},
             {'u', 2}, {'v', 2}, {'w', 2}, {'x', 2}, {'y', 2}, {'z', 2}
         };
-            foreach (KeyValuePair<char, int> pair in this.letters)
-            {
+            foreach(KeyValuePair<char, int> pair in this.letters) {
                 keys.Add(pair.Key);
             }
         }
-          public async void Say(string letter)
-          {
-               var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-               Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(letter);
-               mediaElement.SetSource(stream, stream.ContentType);
-               mediaElement.Play();
-          }
+        public async void Say(string letter) {
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(letter);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             var button = (Button) sender;
@@ -90,37 +90,35 @@ namespace awesomeAlphabetThingy
         }
 
         public async void checkLetter(char letter) {
-            letterBox.Text = letter.ToString();
-            if(letter == Char.ToUpper(current)) {
+            letterBox.Text=letter.ToString();
+            if(letter==Char.ToUpper(current)) {
                 Say("Correct!");
                 await Task.Delay(3000);
-                if (letters[current] == 1)
-                {
+                if(letters[current]==1) {
                     letters.Remove(current);
                     keys.Remove(current);
-                }
-                else
-                {
-                    letters[current] -= 1;
+                } else {
+                    letters[current]-=1;
                 }
                 Next();
-            }else {
+            } else {
                 Say("Lets try this one again");
                 await Task.Delay(3000);
                 Next();
             }
         }
+
+        private void playAgainButton_Click(object sender, RoutedEventArgs e) {
+            Play(current);
+        }
     }
-    public class AudioPlayer
-    {
+    public class AudioPlayer {
         private AudioGraph _graph;
         private Dictionary<string, AudioFileInputNode> _fileInputs = new Dictionary<string, AudioFileInputNode>();
         private AudioDeviceOutputNode _deviceOutput;
 
-        public async Task LoadFileAsync(IStorageFile file)
-        {
-            if (_deviceOutput == null)
-            {
+        public async Task LoadFileAsync(IStorageFile file) {
+            if(_deviceOutput==null) {
                 await CreateAudioGraph();
             }
 
@@ -131,21 +129,19 @@ namespace awesomeAlphabetThingy
             fileInputResult.FileInputNode.AddOutgoingConnection(_deviceOutput);
         }
 
-        public void Play(string key, double gain)
-        {
+        public void Play(string key, double gain) {
             var sound = _fileInputs[key];
-            sound.OutgoingGain = gain;
+            sound.OutgoingGain=gain;
             sound.Seek(TimeSpan.Zero);
             sound.Start();
         }
 
-        private async Task CreateAudioGraph()
-        {
+        private async Task CreateAudioGraph() {
             var settings = new AudioGraphSettings(AudioRenderCategory.Media);
             var result = await AudioGraph.CreateAsync(settings);
-            _graph = result.Graph;
+            _graph=result.Graph;
             var deviceOutputNodeResult = await _graph.CreateDeviceOutputNodeAsync();
-            _deviceOutput = deviceOutputNodeResult.DeviceOutputNode;
+            _deviceOutput=deviceOutputNodeResult.DeviceOutputNode;
             _graph.ResetAllNodes();
             _graph.Start();
         }
