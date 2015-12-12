e73b8a46220384af0b966e18e63893b0859c13ce
Karl <karl.valliste@gmail.com>
2015-12-12 09:48:25 +0200

    Koodi alge

--- awesomeAlphabetThingy/MainPage.xaml.cs
+++ awesomeAlphabetThingy/MainPage.xaml.cs
@@ -22,9 +22,29 @@ namespace awesomeAlphabetThingy
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Dictionary<char, int> letters;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Setup();
+            Intro();
         }
+        public void Intro()
+        {
+
+        }
+        public void Setup()
+        {
+            this.letters = new Dictionary<char, int>
+        {
+            {'a', 4}, {'b', 4}, {'c', 4}, {'d', 4}, {'e', 4}, {'f', 4}, {'g', 4}, {'h', 4}, {'i', 4}, {'j', 4},
+            {'k', 4}, {'l', 4}, {'m', 4}, {'n', 4}, {'o', 4}, {'p', 4}, {'q', 4}, {'r', 4}, {'s', 4}, {'t', 4},
+            {'u', 4}, {'v', 4}, {'w', 4}, {'x', 4}, {'y', 4}, {'z', 4}
+        };
+        }
+
+
+        
     }
-}
+    }
